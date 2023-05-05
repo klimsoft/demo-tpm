14e9d35ae234e81c4ac8765dc4a5caa5f0d014f3
kamil.klimaszewski@gmail.com <kamil.klimaszewski@gmail.com>
2023-05-05 14:25:26 +0200

    IntegrationTest

---
+++ src/test/java/com/example/demotpm/IntegrationTest.java
@@ -0,0 +1,38 @@
+package com.example.demotpm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.net.URL;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class IntegrationTest {
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @LocalServerPort
+    private int port;
+
+    @Test
+    public void testHello() throws Exception {
+        ResponseEntity<String> response = restTemplate.getForEntity(
+                new URL("http://localhost:" + port + "/hello").toString(), String.class);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals("Hello Weekend", response.getBody());
+    }
+
+//    @Test
+//    public void testHelloWithName() throws Exception {
+//        ResponseEntity<String> response = restTemplate.getForEntity(
+//                new URL("http://localhost:" + port + "/hello?name=Ann").toString(), String.class);
+//        assertEquals(HttpStatus.OK, response.getStatusCode());
+//        assertEquals("Hello Ann!", response.getBody());
+//    }
+}
